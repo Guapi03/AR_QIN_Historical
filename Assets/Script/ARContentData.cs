6566446b533e6b89b3c86144460f2986e6169c7a
Guapi03 <brianlim031101@gmail.com>
2026-03-19 17:38:11 +0800

    V1

---
+++ Assets/Script/ARContentData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ARContentData", menuName = "AR/Content Data")]
+public class ARContentData : ScriptableObject
+{
+    public Sprite descriptionImage;
+
+    public AudioClip audioClip;
+}
